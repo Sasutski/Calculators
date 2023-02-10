988ea40178b8a7421d33fe66ce0aeb6b6e0f88be
Sasutski <tan_yu_kai_james@s2022.ssts.edu.sg>
2023-02-10 10:14:17 +0800

    commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Python Calculators
+## Bmi_Calculator
+
+### BMI Calculator is using the Singapore metric system
+
+## Cost Calculator
+
+### Able to input GST and Service Charge as well as splitting the bill.
---
+++ bmi_calculator.py
@@ -0,0 +1,18 @@
+conversion = input("Pounds or Kilograms?:\n")
+if conversion  == ("Kilograms"):
+    weight = float(input("Weight in Kg: \n"))
+    height = float(input("Height in meters: \n"))
+    bmi = (weight/(height ** 2))
+else:
+    weight = float(input("Weight in Pounds: \n"))
+    height = float(input("Height in Inches: \n"))
+    bmi = (weight/(height ** 2)) * 703
+print(bmi)
+if bmi < 18.5:
+    print("Underweight: Possible nutritional deficiency and osteoporosis.")
+elif bmi <= 22.9:
+    print("Healthy Range")
+elif bmi <= 27.4:
+    print("Mild to moderate overweight: Moderate risk of developing heart disease, high blood pressure, stroke, diabetes mellitus.")
+else:
+    print("Very Overweight to obese: High risk of developing heart disease, high blood pressure, stroke, diabetes mellitus. Metabolic Syndrome.")
